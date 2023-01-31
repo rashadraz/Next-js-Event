dea5fa015685b84a2dd89749c9042e4be3f95945
rashad <akyrashad@gmail.com>
2023-01-31 08:33:42 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+
+import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from '../helpers/api-util';
+
+function HomePage(props) {
+ 
+
+  return (
+    <div>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps()
+{
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents
+    },
+    revalidate:1800
+  }
+}
+
+export default HomePage;
